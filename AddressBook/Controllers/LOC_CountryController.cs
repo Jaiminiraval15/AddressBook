c718db1b3dce55dfd8784898f87fccbd97c88025
Jaimini <jaiminiraval123@gmail.com>
2023-08-15 16:18:43 +0530

    Display CountryList

---
+++ AddressBook/Controllers/LOC_CountryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AddressBook.Controllers
+{
+    public class LOC_CountryController : Controller
+    { 
+        private IConfiguration Configuration;
+        public LOC_CountryController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;  
+        }
+        public IActionResult Index()
+        {
+            string str = this.Configuration.GetConnectionString("myConnectionString");
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "SELECTALLOC_Country";
+            DataTable dt = new DataTable();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            
+
+            return View("LOC_CountryList",dt);
+        }
+    }
+}
---
+++ AddressBook/Models/LOC_CountryModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AddressBook.Models
+{
+    public class LOC_CountryModel
+    {
+        public int COUNTRYID { get; set; }
+        public string COUNTRYNAME { get; set; }
+        public string COUNTRYCODE { get; set; }
+        = string.Empty;
+        public DateTime CREATED { get; set; }
+        public DateTime MODIFIED{ get; set; }
+
+
+    }
+}

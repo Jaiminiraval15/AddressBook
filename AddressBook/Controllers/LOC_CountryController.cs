c42a536fab2434ab5daa2ebd9603d596bcb55629
Jaimini <jaiminiraval123@gmail.com>
2023-08-19 14:28:14 +0530

    Edit operation

--- AddressBook/Controllers/LOC_CountryController.cs
+++ AddressBook/Controllers/LOC_CountryController.cs
@@ -43,6 +43,34 @@ namespace AddressBook.Controllers
 
 
         }
+        public IActionResult Add(int? ID)
+
+        {
+            if (ID != null)
+            {
+                string str = this.Configuration.GetConnectionString("myConnectionString");
+                SqlConnection conn = new SqlConnection(str);
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SelectByPKLOC_Country";
+                cmd.Parameters.AddWithValue("@ID", ID);
+                DataTable dt = new DataTable();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                dt.Load(sdr);
+                LOC_CountryModel modelLOC_Country = new LOC_CountryModel();
+                foreach (DataRow dr in dt.Rows)
+                {
+
+                    modelLOC_Country.COUNTRYCODE = dr["COUNTRYCODE"].ToString();
+                    modelLOC_Country.COUNTRYNAME = dr["COUNTRYNAME"].ToString();
+                    //modelLOC_Country.CREATED = Convert.ToDateTime(dr["CREATED"]);
+                    //modelLOC_Country.MODIFIED = Convert.ToDateTime(dr["MODIFIED"]);
+                }
+                return View("LOC_CountryAddEdit", modelLOC_Country);
+            }
+            return View("LOC_CountryAddEdit");
+        }
         [HttpPost]
         public IActionResult Save(LOC_CountryModel modelLOC_Country)
         {
@@ -51,21 +79,42 @@ namespace AddressBook.Controllers
             conn.Open();
             SqlCommand cmd = conn.CreateCommand();
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "InsertLOC_Country";
-            cmd.Parameters.Add("@COUNTRYNAME", SqlDbType.VarChar).Value = modelLOC_Country.COUNTRYNAME;
-            cmd.Parameters.Add("@COUNTRYCODE", SqlDbType.VarChar).Value = modelLOC_Country.COUNTRYCODE;
-            cmd.Parameters.Add("@CREATED", SqlDbType.Date).Value = modelLOC_Country.CREATED;
-            cmd.Parameters.Add("@MODIFIED", SqlDbType.Date).Value = modelLOC_Country.MODIFIED;
+            if(modelLOC_Country.COUNTRYID == 0)
+            {
+                cmd.CommandText = "InsertLOC_Country";
+                cmd.Parameters.AddWithValue("@CREATED", DateTimeOffset.Now);
+            }
+            else
+            {
+                cmd.CommandText = "UPDATELOC_Country";
+                cmd.Parameters.AddWithValue("@COUNTRYID", modelLOC_Country.COUNTRYID);
+            }
+            cmd.Parameters.AddWithValue("@COUNTRYNAME", modelLOC_Country.COUNTRYNAME);
+            cmd.Parameters.AddWithValue("@COUNTRYCODE", modelLOC_Country.COUNTRYCODE);
+           
+            cmd.Parameters.AddWithValue("@MODIFIED", modelLOC_Country.MODIFIED);
+            //cmd.Parameters.Add("@COUNTRYNAME", SqlDbType.VarChar).Value = modelLOC_Country.COUNTRYNAME;
+            //cmd.Parameters.Add("@COUNTRYCODE", SqlDbType.VarChar).Value = modelLOC_Country.COUNTRYCODE;
+            //cmd.Parameters.Add("@CREATED", SqlDbType.Date).Value = modelLOC_Country.CREATED;
+            //cmd.Parameters.Add("@MODIFIED", SqlDbType.Date).Value = modelLOC_Country.MODIFIED;
 
-            cmd.ExecuteNonQuery();
-        
+            if (Convert.ToBoolean(cmd.ExecuteNonQuery()))
+            {
+                if (modelLOC_Country.COUNTRYID == 0)
+                {
+                    TempData["CountryMsg"] = "Record inserted successfully!";
+                }
+                else
+                {
+                    TempData["CountryMsg"] = "Record updated successfully!";
+                    return RedirectToAction("Index");
+                }
+            }
+       
             conn.Close();
 
-            return View("LOC_CountryAddEdit");
+            return RedirectToAction("Add");
         }
-        public IActionResult Add()
-        {
-            return View("LOC_CountryAddEdit");
-        }
+       
     }
 }
--- AddressBook/Models/LOC_CountryModel.cs
+++ AddressBook/Models/LOC_CountryModel.cs
@@ -2,7 +2,7 @@
 {
     public class LOC_CountryModel
     {
-        public int COUNTRYID { get; set; }
+        public int COUNTRYID  { get; set; }
         public string COUNTRYNAME { get; set; }
         public string COUNTRYCODE { get; set; }
         = string.Empty;
